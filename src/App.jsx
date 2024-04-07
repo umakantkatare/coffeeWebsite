0dbf63c51546e170b136ed9ccac3b7a5fe77e3b8
Umakant Katare <umakantkatare9753121329@gmail.com>
2024-04-07 14:58:39 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+
+import { useState } from 'react'
+
+import viteLogo from '/vite.svg'
+
+import reactLogo from './assets/react.svg'
+
+function App() {
+
+  return (
+    <>
+     hello
+    </>
+  )
+}
+
+export default App
